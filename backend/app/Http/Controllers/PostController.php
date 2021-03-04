f911f56da6032de7b17721cb7de6289aaa9aee21
motokitakumi <mtktkm242424@gmail.com>
2021-03-04 20:06:18 +0900

    feat: new routes addition and controller

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -6,5 +6,72 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', ['posts' => $posts]);
+
+    }
+
+    public function create()
+    {
+        
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $id = Auth::id();
+        //インスタンス作成
+        $post = new Post();
+        
+        $post->body = $request->body;
+        $post->user_id = $id;
+
+        $post->save();
+
+       return redirect()->to('/posts');
+    }
+
+    public function show(Post $post)
+    {
+        $usr_id = $post->user_id;
+        $user = DB::table('users')->where('id', $usr_id)->first();
+        
+
+        return view('posts.detail',['post' => $post,'user' => $user]);
+    }
+
+    public function edit($id)
+    {
+        // $usr_id = $post->user_id;
+        $post = \App\Post::findOrFail($id);
+
+        return view('posts.edit',['post' => $post]);
+        // return view('posts.edit');
+    }
+
+    public function update(Request $request)
+    {
+        $id = $request->post_id;
+        
+        //レコードを検索
+        $post = Post::findOrFail($id);
+        
+        $post->body = $request->body;
+        
+        //保存（更新）
+        $post->save();
+        
+        return redirect()->to('/posts');
+    }
+
+    public function destroy($id)
+    {
+        $post = \App\Post::find($id);
+        //削除
+        $post->delete();
+
+        return redirect()->to('/posts');
+    }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('posts', 'PostController', ['only' => ['index','show', 'create', 'store']]);
+Route::get('posts/edit/{id}', 'PostController@edit');
+Route::post('posts/edit', 'PostController@update');
+Route::post('posts/delete/{id}', 'PostController@destroy');
