5a54877e66e037e59298a7355be5d0c24ca0c11e
motokitakumi <mtktkm242424@gmail.com>
2021-03-09 19:12:04 +0900

    delete views in posts

--- backend/resources/views/posts/create.blade.php
+++
@@ -1,6 +0,0 @@
-<form class="" action="{{ route('posts.store') }}" method="POST">
-    {{ csrf_field() }}
-    <div><input class="form-control" type="text" name="title" placeholder="タイトル"></div>
-    <div><textarea id="" class="form-control" name="body" rows="4" placeholder="本文"></textarea></div>
-    <input class="form-control" type="submit" value="送信">
-</form>
